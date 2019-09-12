ac4640c45d51d358ae9cf9263e67948ca6013a11
root <nathaneaston@nathaneaston.com>
2019-09-12 02:06:32 +0000

    Added a python server to run as a daemon. So that stories will be grabbed hourly.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_apscheduler import APScheduler
+ 
+import time
+import InstaClonerService as ICS
+
+app = Flask(__name__)
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+ 
+@app.route('/')
+def welcome():
+    return 'Welcome to flask_apscheduler demo', 200
+ 
+# @app.route('/run-tasks')
+# def run_tasks():
+#     for i in range(10):
+#         app.apscheduler.add_job(func=scheduled_task, trigger='date', args=[i], id='j'+str(i))
+ 
+#     return 'Scheduled several long running tasks.', 200
+
+@scheduler.task('cron', id='stories', hour='*')
+def scheduled_task():
+    driver = ICS.createDriver()
+    config = ICS.loadcfg()
+    try:
+        ICS.login(driver, username_=config['username'],password_=config['password'],totp_key=config['totp_key'])
+    except:
+        ICS.login(driver, username_=config['username'],password_=config['password'])
+    ICS.getStories(driver)
+    print("end")
+         
+app.run(host='0.0.0.0', port=12345)

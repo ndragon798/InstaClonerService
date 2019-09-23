e3425584457fdd497fa56c322a0fdd83a2d0cd13
Nathan Easton <nathaneaston@nathaneaston.com>
2019-09-23 13:56:04 -0400

    Added feature to allow get stories on button push

--- InstaClonerService.py
+++ InstaClonerService.py
@@ -12,6 +12,7 @@ import urllib.request
 from random import randint
 from sys import argv
 
+import onetimepass as otp
 import wget
 import yaml
 from selenium import webdriver
@@ -20,18 +21,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from sqlalchemy import Column, Integer, String, create_engine, LargeBinary, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-import onetimepass as otp
+logging.basicConfig(
+    filename='./InstaClonerService.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    level=logging.INFO)
 
-logging.basicConfig(filename='./InstaClonerService.log',filemode='a',format='%(asctime)s - %(name)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S',level=logging.INFO)
+engine = create_engine("sqlite:///ICS.db",echo=True)
+Base=declarative_base()
+class User(Base):
+    __tablename__ = 'stories'
+    id= Column(String, primary_key=True)
+    last_seen=Column(DateTime)
+
+
 
 def save_cookie(driver, path="cookies.pkl"):
     try:
         pickle.dump(driver.get_cookies(), open(path, "wb"))
-        logging.info('Saved cookies to %s',path)
+        logging.info('Saved cookies to %s', path)
     except Exception as e:
-        logging.error("Error saving cookies to %s",path,exc_info=True)
+        logging.error("Error saving cookies to %s", path, exc_info=True)
         logging.error(str(e))
+
+
 def load_cookie(driver, path="cookies.pkl"):
     try:
         cookies = pickle.load(open(path, "rb"))
@@ -39,39 +56,65 @@ def load_cookie(driver, path="cookies.pkl"):
             if "expiry" in cookie:
                 cookie.pop('expiry', None)
             driver.add_cookie(cookie)
-        logging.info('Loaded cookies from %s',path)
+        logging.info('Loaded cookies from %s', path)
     except Exception as e:
-       logging.error("Error loading cookie at %s",path,exc_info=True)
-       logging.error(str(e))
+        logging.error("Error loading cookie at %s", path, exc_info=True)
+        logging.error(str(e))
 
 
-def find_element_by_name_retry(driver, name, retry_count=5, wait_time=5, killprocess=True):
-    logging.info("Attempting to find element %s with retry count %d",name,retry_count)
+def find_element_by_name_retry(
+        driver,
+        name,
+        retry_count=5,
+        wait_time=5,
+        killprocess=True):
+    logging.info(
+        "Attempting to find element %s with retry count %d",
+        name,
+        retry_count)
     for i in range(retry_count):
         try:
             return driver.find_element_by_name(name)
         except Exception as e:
             time.sleep(wait_time)
             print(e)
-            logging.error("Error finding element %s on attempt number %d",name,i,exc_info=True)
+            logging.error(
+                "Error finding element %s on attempt number %d",
+                name,
+                i,
+                exc_info=True)
     if (killprocess):
         exit()
 
 
-def find_element_by_tag_and_text(driver, tag, text, attribute="innerHTML", multiple=False):
-    logging.info("Trying to find element with tag: %s and text: %s and attribute: %s",tag,text,attribute)
+def find_element_by_tag_and_text(
+        driver,
+        tag,
+        text,
+        attribute="innerHTML",
+        multiple=False):
+    logging.info(
+        "Trying to find element with tag: %s and text: %s and attribute: %s",
+        tag,
+        text,
+        attribute)
     try:
         tags = driver.find_elements_by_tag_name(tag)
         elements = []
         for i in tags:
             if text in i.get_attribute(attribute):
-               if multiple:
-                   elements.append(i)
-               else:
-                   return i
+                if multiple:
+                    elements.append(i)
+                else:
+                    return i
         return elements
     except Exception as e:
-        logging.error("Error finding element with tag: %s and text: %s and attribute: %s",tag,text,attribute,exc_info=True)
+        logging.error(
+            "Error finding element with tag: %s and text: %s and attribute: %s",
+            tag,
+            text,
+            attribute,
+            exc_info=True)
         logging.error(str(e))
 
 
@@ -92,24 +135,29 @@ def createDriver(headless=True):
         logging.info("Created chrome driver")
         return driver
     except Exception as e:
-        logging.error("Error occured during creating of the chrome driver headless: %s",str(headless),exc_info=True)
+        logging.error(
+            "Error occured during creating of the chrome driver headless: %s",
+            str(headless),
+            exc_info=True)
         logging.error(str(e))
 
 
-def login(driver, username_ = None,password_ = None,totp_key = None):
+def login(driver, username_=None, password_=None, totp_key=None):
     driver.get('https://www.instagram.com/accounts/login/')
     load_cookie(driver)
     driver.get('https://www.instagram.com/accounts/login/')
-        # Read in username and password for instagram
+    # Read in username and password for instagram
     if (driver.current_url == "https://www.instagram.com/accounts/login/"):
-        if (not username_  or not password_):
+        if (not username_ or not password_):
             username_ = input("Please Input Username: ").strip()
             password_ = getpass.getpass("Please Input Password: ").strip()
         try:
             if driver.current_url == "https://www.instagram.com/accounts/login/":
                 # Type in login
-                find_element_by_name_retry(driver, 'username').send_keys(username_)
-                find_element_by_name_retry(driver, 'password').send_keys(password_)
+                find_element_by_name_retry(
+                    driver, 'username').send_keys(username_)
+                find_element_by_name_retry(
+                    driver, 'password').send_keys(password_)
                 webdriver.ActionChains(driver).send_keys(Keys.ENTER).perform()
                 # Wait for login
                 time.sleep(5)
@@ -118,53 +166,66 @@ def login(driver, username_ = None,password_ = None,totp_key = None):
                     if not totp_key:
                         two_fa_code = input("Please Enter 2FA Code: ").strip()
                     else:
-                        logging.info("Attempting to create 2fa code using config key")
+                        logging.info(
+                            "Attempting to create 2fa code using config key")
                         two_fa_code = otp.get_totp(totp_key)
-                        logging.info("Created 2FA Code: %s",two_fa_code)
-                    find_element_by_tag_and_text(driver,'input',"Security Code","aria-label").send_keys(two_fa_code) 
-                    webdriver.ActionChains(driver).send_keys(Keys.ENTER).perform()
-                #Wait for login
+                        logging.info("Created 2FA Code: %s", two_fa_code)
+                    find_element_by_tag_and_text(
+                        driver, 'input', "Security Code", "aria-label").send_keys(two_fa_code)
+                    webdriver.ActionChains(driver).send_keys(
+                        Keys.ENTER).perform()
+                # Wait for login
                 time.sleep(5)
-                logging.info("Logged in as %s",username_)
+                logging.info("Logged in as %s", username_)
                 # Save cookie
                 save_cookie(driver)
         except Exception as e:
-            logging.error("Failed to login as %s",username_,exc_info=True)
+            logging.error("Failed to login as %s", username_, exc_info=True)
             logging.error(str(e))
-            driver.save_screenshot(str(time.time())+".png")
+            driver.save_screenshot(str(time.time()) + ".png")
     return username_
 
 
 def getFollowerList(driver, username_):
     logging.info("Attempting to get Follower List")
-    username_ = driver.find_element_by_xpath('//section/div/div/div/div/div/a').get_attribute("innerHTML")
+    username_ = driver.find_element_by_xpath(
+        '//section/div/div/div/div/div/a').get_attribute("innerHTML")
     # Go to profile
-    driver.get('https://www.instagram.com/'+username_)
+    driver.get('https://www.instagram.com/' + username_)
     # link.click()
     time.sleep(3)
     # Get following amount
-    follower_amount_class_id = find_element_by_tag_and_text(driver, 'a', 'following').get_attribute('innerHTML').split('"')[1].strip()
-    follower_amount = find_element_by_tag_and_text(driver, 'span', follower_amount_class_id, 'class', True)[2].get_attribute('innerHTML')
+    follower_amount_class_id = find_element_by_tag_and_text(
+        driver, 'a', 'following').get_attribute('innerHTML').split('"')[1].strip()
+    follower_amount = find_element_by_tag_and_text(
+        driver,
+        'span',
+        follower_amount_class_id,
+        'class',
+        True)[2].get_attribute('innerHTML')
     # Open following tab
     find_element_by_tag_and_text(driver, 'a', 'following', 'href').click()
     time.sleep(5)
-    usernames_elements = driver.find_elements_by_xpath("//li/div/div/div/div/a")
+    usernames_elements = driver.find_elements_by_xpath(
+        "//li/div/div/div/div/a")
     usernames = []
     while (len(usernames_elements) < int(follower_amount)):
         webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
-        usernames_elements = driver.find_elements_by_xpath("//li/div/div/div/div/a")
+        usernames_elements = driver.find_elements_by_xpath(
+            "//li/div/div/div/div/a")
     for names in usernames_elements:
         usernames.append(names.text)
-    logging.info("Got list of users length %d",len(usernames))
+    logging.info("Got list of users length %d", len(usernames))
     return usernames
 
-def downloadStoryFile(url,download_folder):
+
+def downloadStoryFile(url, download_folder):
     # Download the file from `url` and save it locally under `file_name`:
-    logging.info("Attempting to download %s to %s",url,download_folder)
+    logging.info("Attempting to download %s to %s", url, download_folder)
     print(download_folder)
-    if not os.path.exists('static/stories/'+download_folder):
-        os.makedirs('static/stories/'+download_folder)
-    wget.download(url,out='static/stories/'+download_folder+'/')
+    if not os.path.exists('static/stories/' + download_folder):
+        os.makedirs('static/stories/' + download_folder)
+    wget.download(url, out='static/stories/' + download_folder + '/')
 
 
 def getStories(driver):
@@ -173,21 +234,22 @@ def getStories(driver):
     download_threads = []
     driver.get("https://www.instagram.com/")
     time.sleep(3)
-    #check for button to enable notifications
+    # check for button to enable notifications
     try:
-        find_element_by_tag_and_text(driver,'button','Not Now').click()
-    except:
+        find_element_by_tag_and_text(driver, 'button', 'Not Now').click()
+    except BaseException:
         pass
     time.sleep(2)
     try:
-        find_element_by_tag_and_text(driver,'a',"Watch All").click()
-    except:
+        find_element_by_tag_and_text(driver, 'a', "Watch All").click()
+    except BaseException:
         # driver.set_window_size(1920,1080)
         try:
-            find_element_by_tag_and_text(driver,'button',"menuitem",'role').click()
+            find_element_by_tag_and_text(
+                driver, 'button', "menuitem", 'role').click()
         except Exception as e:
             logging.error(e)
-            driver.save_screenshot(str(time.time())+".png")   
+            driver.save_screenshot(str(time.time()) + ".png")
             sys.exit(1)
     while(driver.current_url == "https://www.instagram.com/"):
         time.sleep(1)
@@ -197,44 +259,58 @@ def getStories(driver):
         try:
             # stories[driver.current_url].update(find_element_by_tag_and_text(driver,'source','video/mp4','type').get_attribute('src'))
             # stories[driver.current_url].add(find_element_by_tag_and_text(driver,'img','sync',"decoding").get_attribute('src'))
-            url=find_element_by_tag_and_text(driver,'img','sync',"decoding").get_attribute('src')
+            url = find_element_by_tag_and_text(
+                driver, 'img', 'sync', "decoding").get_attribute('src')
             print(url)
-            download_threads.append(threading.Thread(target=downloadStoryFile,args=(url,driver.current_url.split('/')[-2])))
+            download_threads.append(threading.Thread(
+                target=downloadStoryFile, args=(url, driver.current_url.split('/')[-2])))
             download_threads[-1].start()
-            amt_downloaded+=1
-            find_element_by_tag_and_text(driver,'div','coreSpriteRightChevron','class').click()
+            amt_downloaded += 1
+            find_element_by_tag_and_text(
+                driver, 'div', 'coreSpriteRightChevron', 'class').click()
         except AttributeError as e:
             # stories[driver.current_url].update(find_element_by_tag_and_text(driver,'video','auto','preload'))
             print(e)
-        except Exception as e:            
+        except Exception as e:
             print(e)
     for t in download_threads:
         t.join()
-    logging.info("Got %d stories",amt_downloaded)
+    logging.info("Got %d stories", amt_downloaded)
 
 
 def loadcfg(settings_file='./settings.yml'):
-    logging.info("Attempting to load cfg from %s",settings_file)
+    logging.info("Attempting to load cfg from %s", settings_file)
     return yaml.safe_load(open(settings_file))
+
 
 def endChrome(driver):
     logging.info("Exiting driver instance.")
     try:
         driver.close()
         driver.quit()
-    except:
+    except BaseException:
         logging.error(exc_info=True)
+
+
 def main():
     logging.info("Starting Main Method")
     driver = createDriver()
     config = loadcfg()
     try:
-        login(driver, username_=config['username'],password_=config['password'],totp_key=config['totp_key'])
-    except:
-        login(driver, username_=config['username'],password_=config['password'])
+        login(
+            driver,
+            username_=config['username'],
+            password_=config['password'],
+            totp_key=config['totp_key'])
+    except BaseException:
+        login(
+            driver,
+            username_=config['username'],
+            password_=config['password'])
     getStories(driver)
     print("end")
     # print(getFollowerList(driver, username_))
+
 
 if __name__ == "__main__":
     main()
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python3
+import base64
 import os
 import time
-import base64
+import threading
 from importlib import reload
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_apscheduler import APScheduler
 
 import InstaClonerService as ICS
@@ -14,19 +15,31 @@ scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
- 
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        t=threading.Thread(target=scheduled_task)
+        t.start()
+        return render_template('index.html')
+
 @app.route('/<user>')
 def usergallery(user=None):
-    photosloc=[]
-    for (_root, _dirs, files) in os.walk("static/stories/"+user):
+    photosloc = []
+    for (_root, _dirs, files) in os.walk("static/stories/" + user):
         for file in files:
-            with open(_root+"/"+file,"rb") as img_file:
-               photosloc.append(base64.b64encode(img_file.read()).decode('utf-8'))
-            #photosloc.append("stories/"+user+"/"+file)
-    return render_template('user.html',photos=photosloc,photolen=len(photosloc))
+            with open(_root + "/" + file, "rb") as img_file:
+                photosloc.append(
+                    base64.b64encode(
+                        img_file.read()).decode('utf-8'))
+            # photosloc.append("stories/"+user+"/"+file)
+    return render_template(
+        'user.html',
+        photos=photosloc,
+        photolen=len(photosloc))
+
 
 @scheduler.task('cron', id='stories', hour='*')
 def scheduled_task():
@@ -34,11 +47,19 @@ def scheduled_task():
     driver = ICS.createDriver()
     config = ICS.loadcfg()
     try:
-        ICS.login(driver, username_=config['username'],password_=config['password'],totp_key=config['totp_key'])
-    except:
-        ICS.login(driver, username_=config['username'],password_=config['password'])
+        ICS.login(
+            driver,
+            username_=config['username'],
+            password_=config['password'],
+            totp_key=config['totp_key'])
+    except BaseException:
+        ICS.login(
+            driver,
+            username_=config['username'],
+            password_=config['password'])
     ICS.getStories(driver)
     print("end")
     ICS.endChrome(driver)
-         
-app.run(host='0.0.0.0', port=12345)
+
+
+app.run(host='0.0.0.0', port=12345,threaded=True,debug=True)
